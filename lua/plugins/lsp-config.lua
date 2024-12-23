7d5118789fb37cad23ee63997148dd4c2221bd19
Vasu Bansal <vasubansal1998@gmail.com>
2024-12-23 14:41:54 +0530

    [Vasu] added autocompletion and lsp plugins

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -17,7 +17,11 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
